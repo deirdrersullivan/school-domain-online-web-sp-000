8b9b22e8ff275c75c1539572bb0ce8cb00e8034d
Deirdre  Sullivan <dsulliv4@binghamton.edu>
2019-09-14 00:33:27 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -15,10 +15,12 @@ class School
     roster[student_grade]
   end
 
-def sort 
-  sorted = {}
-  roster.each do |grade,students|
-    sorted[grade]= students.sort
+  # this method should arrange the students in each grade by alphabetical order
+  def sort
+    sorted = {}
+    roster.each do |grade, students|
+      sorted[grade] = students.sort
+    end
+    sorted
   end
-sorted
 end
